d1afe9d4c7337a1a45691852f78c1731add719f9
Morteza Bokharaie <morteza.bokharaie@gmail.com>
2016-04-12 11:51:44 +1000

    Test for AdditiveExpression

--- src/test/java/org/ifn660/jflexer/TestCases.java
+++ src/test/java/org/ifn660/jflexer/TestCases.java
@@ -14,7 +14,8 @@ public class TestCases {
                 "TestCompilationUnit2.java", "TestCompilationUnit3.java",
                 "TestPass1.java", "TestPass2.java", "TestPass3.java",
                 "TestPass4.java", "TestBlockStatement.java", "TestBlockStatement2.java",
-                "TestEmptyTypeDeclaration.java","TestEmptyClassBody.java"};
+                "TestEmptyTypeDeclaration.java","TestEmptyClassBody.java",
+                "TestAdditiveExpression.java"};
 
         final int expectedPass = filenames.length;
         int actualPass = 0;
