d2f2ac1f4b455ca3f91de6657e71ab6889b89db6
Corcatraz <tpcorc@yahoo.com.au>
2016-04-17 12:09:46 +1000

    added parameters and modifiers as a List

--- src/main/java/org/ifn660/jflexer/ast/ASTDriver.java
+++ src/main/java/org/ifn660/jflexer/ast/ASTDriver.java
@@ -1,13 +1,22 @@
 package org.ifn660.jflexer.ast;
 
+import java.util.ArrayList;
+
 public class ASTDriver {
     public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
+    	ArrayList<String> modifiers = new ArrayList<String>();
+    	modifiers.add("public");
+    	modifiers.add("static");
+    	
+    	ArrayList<String> parameters = new ArrayList<String>();
+    	parameters.add("args");
+    	
     	Expression expression = new Expression(42);
     	VariableInitializer varInit = new VariableInitializer("=", expression);
     	VariableDeclaratorId varDecId = new VariableDeclaratorId("x");
     	LocalVariableDeclarationStatement localVarDec = new LocalVariableDeclarationStatement(varDecId, varInit);
     	MethodBody methodBod = new MethodBody(localVarDec);
-    	MethodDeclaration methodDec = new MethodDeclaration("public", "void", "main", methodBod);
+    	MethodDeclaration methodDec = new MethodDeclaration(modifiers, "void", "main", parameters, methodBod);
     	ClassBody classBod = new ClassBody(methodDec);
   
     	
--- src/main/java/org/ifn660/jflexer/ast/MethodDeclaration.java
+++ src/main/java/org/ifn660/jflexer/ast/MethodDeclaration.java
@@ -1,16 +1,19 @@
 package org.ifn660.jflexer.ast;
 
+import java.util.List;
+
 public class MethodDeclaration extends Node {
-	private String modifier;
+	private List modifiers;
 	private String result;
 	private String name;
-	private String parameter;
+	private List parameters;
 	private MethodBody methodBod;
 	
-	public MethodDeclaration (String modifier, String result, String name, MethodBody methodBod) {
-		this.modifier = modifier;
+	public MethodDeclaration (List modifiers, String result, String name, List parameters, MethodBody methodBod) {
+		this.modifiers = modifiers;
 		this.result = result;
 		this.name = name;
+		this.parameters = parameters;
 		this.methodBod = methodBod;
 	}
 }
