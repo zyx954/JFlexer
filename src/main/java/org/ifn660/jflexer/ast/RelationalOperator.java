c6a17e3f9f67ebb1bb8c28c1ea701bcbb765a4f9
marshall <zyx954@gmail.com>
2016-05-13 21:26:28 +1000

    delete RelationlExpression & RelationlOperator By using BinaryExpression & BinaryOperator

--- src/main/java/org/ifn660/jflexer/ast/RelationalOperator.java
+++
@@ -1,9 +0,0 @@
-package org.ifn660.jflexer.ast;
-
-public class RelationalOperator extends Node{
-    private String operator;
-
-    public RelationalOperator(String operator) {
-        this.operator = operator;
-    }
-}
