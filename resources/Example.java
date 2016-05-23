04346b362d62e2681e254ad92232cc4db5d7251e
vthai <veng.thai@gmail.com>
2016-05-23 20:30:20 +1000

    Add in the method invocation

--- resources/Example.java
+++ resources/Example.java
@@ -2,6 +2,7 @@
     //private int z;//zyx:here z is not static,So we cannot use z in method body.
     public static void main(String[] args) {
         int x = 42;
+        System.out.println(x);
 //        int y;
 //        int f = 4;
 //        y = x + 4;
